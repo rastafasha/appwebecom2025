4ec5d4a7ebef3fa02b73b329324b624d6d1431fc
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-06-17 12:56:36 -0400

    refactor: enhance experience component layout and improve option selection functionality

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
-  imports: [],
+  imports: [
+    CommonModule,
+    
+  ],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss'
 })
 export class ExperienceComponent {
+
+  option_selected:number = 1;
+  solicitud_selected:any = null;
+
+
+  optionSelected(value:number){
+      this.option_selected = value;
+      if(this.option_selected === 1){
+
+        // this.ngOnInit();
+        this.solicitud_selected = null;
+      }
+      if(this.option_selected === 2){
+        this.solicitud_selected = null;
+      }
+      if(this.option_selected === 3){
+        this.solicitud_selected = null;
+      }
+      if(this.option_selected === 4){
+        this.solicitud_selected = null;
+      }
+      if(this.option_selected === 5){
+        this.solicitud_selected = null;
+      }
+    }
 
 }
