41bd4ae5d7431026ccf26a6ad130749672273470
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-09-23 11:49:32 -0400

    modal producto, splashscreen, logo a curvas

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,15 +1,14 @@
 import { CommonModule, NgFor, NgIf } from '@angular/common';
-import { Component, inject, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Galeria } from '../../models/galeria.model';
-import { Producto } from '../../models/producto.model';
 import { ImagenPipe } from '../../pipes/imagen-pipe.pipe';
-import { GaleriaService } from '../../services/galeria.service';
 
 @Component({
   selector: 'app-modal',
   imports: [
     CommonModule,
     NgFor,
+    NgIf,
     ImagenPipe
   ],
   templateUrl: './modal.component.html',
@@ -17,36 +16,8 @@ import { GaleriaService } from '../../services/galeria.service';
 })
 export class ModalComponent {
   @Input() selectedProduct!: Galeria;
-  public galeria!: Galeria[]|null;
-  private _galeriaService= inject(GaleriaService)
-  public img_select:any;
-  public first_img:any;
 
   ngOnInit(){
-    console.log(this.selectedProduct);
-    // this.getGalleryProducto();
+    console.log('Modal opened with product:', this.selectedProduct);
   }
-
-  getGalleryProducto(){
-    if (this.selectedProduct && this.selectedProduct._id) {
-      this._galeriaService.find_by_product(this.selectedProduct._id).subscribe(
-        response =>{
-          this.galeria = []; // Ensure galeria is an array before pushing
-          response.galeria.forEach((element: { imagen: any; _id: any; },index: number) => {
-            if(index == 0){
-              this.first_img = element.imagen;
-            }
-              // this.galeria.push({_id:element._id,imagen : element.imagen});
-          });
-        },
-        error=>{
-          console.log(error);
-
-        }
-      );
-    } else {
-      console.error('selectedProduct._id is undefined');
-    }
-  }
-
 }
